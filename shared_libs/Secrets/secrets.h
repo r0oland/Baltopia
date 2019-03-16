7635d634d4787bff98678d8edee004efb10515f5
Johannes Rebling <johannesrebling@gmail.com>
2019-03-16 21:30:26 +0100

    huzzah + oled playing around

--- shared_libs/Secrets/secrets.h
+++ shared_libs/Secrets/secrets.h
@@ -3,9 +3,12 @@
 
 // #define SECRET_SSID "Spam"                                // replace MySSID with your WiFi network name
 // #define SECRET_PASS "12345678"                            // replace MyPassword with your WiFi password
+// 
+// #define SECRET_SSID "RazanskyLab"                                // replace MySSID with your WiFi network name
+// #define SECRET_PASS "WeLoveOptoacoustics"                            // replace MyPassword with your WiFi password
 
-#define SECRET_SSID "RazanskyLab"                                // replace MySSID with your WiFi network name
-#define SECRET_PASS "WeLoveOptoacoustics"                            // replace MyPassword with your WiFi password
+#define SECRET_SSID "Kassiopeia"                                // replace MySSID with your WiFi network name
+#define SECRET_PASS "SternbilddesNordhimmels"                            // replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 712441                                 // replace 0000000 with your channel number
 #define SECRET_WRITE_APIKEY "KJI9O0E9U3E0VD0K"                           // replace XYZ with your channel write API Key
